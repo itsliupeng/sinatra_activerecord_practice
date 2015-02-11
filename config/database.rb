eb9375a5130bfc87e66a9d179c22fc2ea14cacd2
liupeng <itsliupeng@gmail.com>
2015-02-12 00:44:56 +0800

    use set "databse" in config/database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,3 +1,10 @@
 require 'active_record'
+require 'sinatra/activerecord'
 
-ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: 'foobar.db'
+# ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: 'foobar.db'
+
+# configure :development do
+#   ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: 'foobar.db'
+# end
+
+set :database, {adapter: "sqlite3", database: "foobar.db"}
